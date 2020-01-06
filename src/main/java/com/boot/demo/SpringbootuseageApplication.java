d0a345c0a6a72283cfb5154b6d5ae2b02b707902
liuzhendong <952599783@qq.com>
2020-01-06 17:36:32 +0800

    Initial commit

---
+++ src/main/java/com/boot/demo/SpringbootuseageApplication.java
@@ -0,0 +1,13 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootuseageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootuseageApplication.class, args);
+    }
+
+}
