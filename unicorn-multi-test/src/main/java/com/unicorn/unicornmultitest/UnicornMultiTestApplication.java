c0ff6c892e87f5c7dfd5b08f2f21d9d07206561b
liuzhendong <952599783@qq.com>
2020-09-22 10:48:57 +0800

    dev local commit without push

--- unicorn-multi-test/src/main/java/com/unicorn/unicornmultitest/UnicornMultiTestApplication.java
+++ unicorn-multi-test/src/main/java/com/unicorn/unicornmultitest/UnicornMultiTestApplication.java
@@ -13,6 +13,8 @@ public class UnicornMultiTestApplication {
         SpringApplication.run(UnicornMultiTestApplication.class, args);
 //        HelloServer helloServer = new HelloServer();
 //        helloServer.start(new InetSocketAddress("127.0.0.1", 8088));
+
+        // git test merge dev branch into master by local commit
     }
 
 }
